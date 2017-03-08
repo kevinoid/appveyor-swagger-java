dfc03d588d024883e80d209276e90fbbb937f9c5
Kevin Locke <kevin@kevinlocke.name>
2017-03-08 16:24:39 -0700

    Log API endpoint being used for tests
    To aid debugging.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- src/test/java/name/kevinlocke/appveyor/testutils/TestApiClient.java
+++ src/test/java/name/kevinlocke/appveyor/testutils/TestApiClient.java
@@ -1,6 +1,12 @@
 package name.kevinlocke.appveyor.testutils;
 
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.util.List;
+
+import org.testng.Reporter;
 
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
@@ -18,6 +24,29 @@ public final class TestApiClient {
 		}
 
 		testApiClient = new ApiClient();
+
+		// Log the API endpoint being used
+		StringBuilder apiEndpointMsg = new StringBuilder(
+				"Using API endpoint: ");
+		String basePath = testApiClient.getBasePath();
+		apiEndpointMsg.append(basePath);
+		try {
+			String apiHost = new URI(basePath).getHost();
+			InetAddress[] addresses = InetAddress.getAllByName(apiHost);
+			apiEndpointMsg.append(" (");
+			for (InetAddress address : addresses) {
+				apiEndpointMsg.append(address.getHostAddress()).append(", ");
+			}
+			apiEndpointMsg.delete(apiEndpointMsg.length() - 2,
+					apiEndpointMsg.length());
+			apiEndpointMsg.append(')');
+		} catch (UnknownHostException e) {
+			Reporter.log("Unable to resolve API host: " + e, 1, true);
+		} catch (URISyntaxException e) {
+			Reporter.log("Unable to parse API basepath: " + e, 1, true);
+		}
+		Reporter.log(apiEndpointMsg.toString(), 1, true);
+
 		testApiClient.setApiKeyPrefix("Bearer");
 		testApiClient.setApiKey(apiToken);
 
