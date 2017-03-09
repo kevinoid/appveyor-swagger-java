dd60c236da7991fb6e7a4766487910a4189ac553
Kevin Locke <kevin@kevinlocke.name>
2017-03-08 20:14:07 -0700

    Add content matching for SVG assertion
    URLConnection.guessContentTypeFromStream can recognize XML content, but
    not SVG specifically.  Add namespace check to cover this case.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- src/test/java/name/kevinlocke/appveyor/testutils/AssertMediaType.java
+++ src/test/java/name/kevinlocke/appveyor/testutils/AssertMediaType.java
@@ -1,11 +1,14 @@
 package name.kevinlocke.appveyor.testutils;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -44,22 +47,51 @@ public class AssertMediaType {
 		return mediaType.trim();
 	}
 
-	public static void assertIsPng(InputStream is, Path path)
-			throws IOException {
+	public static void assertMediaType(InputStream is, Path path,
+			String expectedType) throws IOException {
 		String contentType = trimParams(guessMediaType(is, path));
-		assertEquals(contentType, "image/png");
+		assertEquals(contentType, expectedType);
+	}
+
+	public static void assertMediaType(Path path, String expectedType)
+			throws IOException {
+		try (BufferedInputStream bis = new BufferedInputStream(
+				Files.newInputStream(path))) {
+			assertMediaType(bis, path, expectedType);
+		}
 	}
 
 	public static void assertIsPng(Path path) throws IOException {
-		try (BufferedInputStream bis = new BufferedInputStream(
-				Files.newInputStream(path))) {
-			assertIsPng(bis, path);
-		}
+		assertMediaType(path, "image/png");
 	}
 
 	public static void assertIsSvg(InputStream is, Path path)
 			throws IOException {
 		String contentType = trimParams(guessMediaType(is, path));
+
+		// URLConnection.guessContentTypeFromStream recognizes XML from the XML
+		// declaration and doesn't recognize SVG specifically. Check xmlns.
+		if (contentType == null || contentType.equals("application/xml")
+				|| contentType.equals("text/xml")) {
+			char[] searchBuf = new char[512];
+			is.mark(searchBuf.length);
+			// Need to use standard single-byte charset compatible with ASCII.
+			InputStreamReader reader = new InputStreamReader(is,
+					StandardCharsets.ISO_8859_1);
+			int nRead;
+			try {
+				nRead = reader.read(searchBuf);
+			} finally {
+				is.reset();
+			}
+			String searchStr = new String(searchBuf, 0, nRead);
+			// Note: There are lots of ways to make this check tighter.
+			// This works for the current use case for now. Improve as needed.
+			assertTrue(searchStr.indexOf("http://www.w3.org/2000/svg") >= 0,
+					"Content contains SVG namespace");
+			return;
+		}
+
 		assertEquals(contentType, "image/svg+xml");
 	}
 
